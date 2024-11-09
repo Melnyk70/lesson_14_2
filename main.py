9959556b93b0d3b89f7132d83a9b7918499b8bff
MelnykIhor <melnyk70@gmail.com>
2024-11-09 12:10:38 +0200

    DZ_14_2

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ДЗ 14.2. Створення власних модулів
+У цьому завданні Вам необхідно зробити дві речі на підставі попереднього ДЗ.
+До класу Студента необхідно додати метод порівняння. Порівнювати можна за тими рядками, які повертає метод str Для того, щоб спрацювала коректно ось ця перевірка
+assert gr.find_student('Jobs') == st1
+Рознесіть класи, які використовували під час виконання завдання про групу студентів за модулями.
+Переконайтеся у працездатності проекту – створіть окремо файл main.py, у якому необхідно імпортувати потрібні класи та запустити код перевірки.
+Приблизний вміст файлу main.py для перевірки працездатності.
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert gr.find_student('Jobs') == st1  # 'Steve Jobs'
+assert gr.find_student('Jobs2') is None
+gr.delete_student('Taylor')
+print(gr) # Only one student
+Результат оформить у вигляді zip-архіву, в якому будуть зібрані всі необхідні модулі.
+Якщо при спробі додавання студента до групи ви побачите помилку
+self.group.add(student)
+TypeError: unhashable type: 'Student'
+то додайте до класу студента метод hash
+def __hash__(self):
+    return hash(str(self))
